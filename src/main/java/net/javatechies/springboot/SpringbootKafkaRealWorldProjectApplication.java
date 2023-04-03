19f84d4a4d0efcaadbf7b6645bc8289946f6474a
Rajesh Puppala <rajeshpuppala88@gmail.com>
2023-04-02 23:30:25 -0400

    Initial commit

---
+++ src/main/java/net/javatechies/springboot/SpringbootKafkaRealWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package net.javatechies.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaRealWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaRealWorldProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javatechies/springboot/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javatechies.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
